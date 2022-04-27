3c5aa5218bf43668ae6dc75a89cd74c795664a58
Hani AYADI <hani78120@gmail.com>
2022-04-27 14:58:36 +0200

    kata1cypress

--- cypress/integration/KATA01.spec.js
+++ cypress/integration/KATA01.spec.js
@@ -3,5 +3,23 @@
 describe("Actions", () => {
   it("scenario 1", () => {
     cy.visit("index.html");
+    cy.get('[data-cy="Q1"]').should("be.visible");
+    cy.get('[data-cy="Q2"]').should("be.visible");
+    cy.get('[data-cy="test1"]').should("be.visible");
+    cy.get('[data-cy="test2bis"]').should("be.visible");
+    cy.get('[data-cy="test3bis"]').should("be.visible");
+    cy.get('[data-cy="test4bis"]').should("be.visible");
+
+    cy.get("#pet-select").select("france").should("have.value", "france");
+  });
+  it("scenario 2", () => {
+    cy.visit("index.html");
+    cy.get('[data-cy="test4"]').click();
+    cy.get('[data-cy="bouton valider"]').click();
+  });
+  it("scenario 3", () => {
+    cy.visit("index.html");
+    cy.get('[data-cy="test3"]').click();
+    cy.get('[data-cy="bouton valider"]').click();
   });
 });
--- cypress/integration/demoqa.spec.js
+++ cypress/integration/demoqa.spec.js
@@ -1,7 +1,16 @@
 /// <reference types="cypress"/>
+const { faker } = require("@faker-js/faker");
+const name = faker.name.findName();
+const email = faker.internet.email();
 
 describe("Actions", () => {
   it("scenario 1", () => {
-    cy.visit("demoqa.com");
+    cy.visit("https://demoqa.com/text-box");
+    cy.get("#userName").type(name);
+    cy.get("#userEmail").type(email);
+    cy.get("#submit").click();
+
+    cy.get("[id=name]").should("be.visible").and("include.text", name);
+    cy.get("#email").should("be.visible").and("contains.text", email);
   });
 });
